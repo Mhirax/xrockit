ad33bf43277865b7cb334562e531e0dc3cd915e3
Mhirax <olowookeremiracle14@gmail.com>
2025-12-14 14:35:56 +0100

    made and added play control changes

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,8 +7,10 @@ const player = () => {
           <p>Start Time</p>
           <input type="range" />
           <p>End Time</p>
-</div>
+        </div>
+        <div className="play control"></div>
       </div>
+      
     );
 }
 
