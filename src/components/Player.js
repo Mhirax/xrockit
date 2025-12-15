0eb5d93fbbad89033c4417e384b328cae37e23d8
Mhirax <olowookeremiracle14@gmail.com>
2025-12-15 13:43:28 +0100

    import icons and make changes

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,11 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
 
 const Player = () => {
   return (
@@ -9,7 +15,11 @@ const Player = () => {
         <input type="range" />
         <p>End Time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" icon={faPlay} />
+        <FontAwesomeIcon className="skip-forward" icon={faAngleRight} />
+      </div>
     </div>
   );
 };
