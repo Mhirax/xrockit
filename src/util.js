02b0d460b544fa4d15f5f8324dd1ea61691fa065
Mhirax <olowookeremiracle14@gmail.com>
2025-12-17 18:55:47 +0100

    make changes to my data files

---
+++ src/util.js
@@ -0,0 +1,16 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillhop() {
+    return [
+        {
+            name:"Beaver creek",
+            cover:,
+            arists:,
+            audio:,
+            color:,
+            id:,
+            active:,
+            
+        }
+    ]
+}
