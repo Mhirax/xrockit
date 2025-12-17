e5a763afadfc33944420694aa82a95e1bbe2c5d8
Mhirax <olowookeremiracle14@gmail.com>
2025-12-17 19:38:19 +0100

    made changes to my song.js app

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,8 +3,8 @@ import React from "react";
 const Song = ({currentSong}) => {
   return (
     <div className="song-container">
+      <h1>{currentSong.cover}</h1>
       <h1></h1>
-      <h1>Song Name</h1>
       <h1>Artist </h1>
     </div>
   );
