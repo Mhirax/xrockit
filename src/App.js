4255de2fd65f7c45470b22c47eedd9484667ec7f
Mhirax <olowookeremiracle14@gmail.com>
2025-12-17 19:20:21 +0100

    import our data app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./styles/app.scss"
 //Adding components 
 import Player from './components/Player';
 import Song from './components/Song';
+//import utill
+import data from "./util";
 
 function App() {
   return (
--- src/util.js
+++ src/util.js
@@ -4,10 +4,10 @@ function chillhop() {
     return [
         {
             name:"Beaver creek",
-            cover:,
-            arists:,
+            cover:"https://cms.chillhop.com/media/87042/squarel33849952536cf31df73251c84815da650404c44d.jpg",
+            arists:"Monwa",
             audio:,
-            color:,
+            color:[""],
             id:,
             active:,
             
