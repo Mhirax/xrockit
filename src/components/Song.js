0a0e507f9922f83856ff58815d5edbbbdea6e61c
Mhirax <olowookeremiracle14@gmail.com>
2025-12-17 19:36:08 +0100

    remove the picture in h1 tag

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,7 +3,7 @@ import React from "react";
 const Song = ({currentSong}) => {
   return (
     <div className="song-container">
-      <h1>Picture</h1>
+      <h1></h1>
       <h1>Song Name</h1>
       <h1>Artist </h1>
     </div>
