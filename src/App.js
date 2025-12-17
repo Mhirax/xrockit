d0ecd5e4667b507f7337da58f42a9d0c6735dbd9
Mhirax <olowookeremiracle14@gmail.com>
2025-12-17 19:30:11 +0100

    added our current song variable

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import data from "./util";
 
 function App() {
   const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0])
   return (
     <div className="App">
       <Song />
