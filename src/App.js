a78d1b2d5dd18314f050d5f8c67b80044790de3e
Mhirax <olowookeremiracle14@gmail.com>
2025-12-18 23:21:13 +0100

    adding state to make our play icon work

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import data from "./util";
 function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
