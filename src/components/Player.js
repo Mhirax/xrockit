4248beb06a3d1a8b0f80bee1df9e6389e546fbc1
Mhirax <olowookeremiracle14@gmail.com>
2025-12-18 22:38:33 +0100

    import useRef in our player,js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faPlay,
@@ -10,7 +10,7 @@ import {
 const Player = ({ currentSong }) => {
   //Events Handler
   const playSongHandler = () => {
-    
+
   }
   return (
     <div className="player">
