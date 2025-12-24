01de8708dacb96fa0b87accf1d15f455bdc56fb3
Mhirax <olowookeremiracle14@gmail.com>
2025-12-24 06:55:43 +0100

    we print out our event

--- src/components/Player.js
+++ src/components/Player.js
@@ -27,6 +27,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   const timeUpateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
+    console.log(current);
 };
 
   //state
