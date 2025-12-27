cbe0dc73465a3d12864d7e9edcd5065764246498
Mhirax <olowookeremiracle14@gmail.com>
2025-12-27 08:08:47 +0100

    passing our libarySongs down to our library

--- src/components/Library.js
+++ src/components/Library.js
@@ -6,7 +6,7 @@ const = () => {
         <div className="Library">
             <h2>Library</h2>
             <div className="Library-Songs">
-
+               <LibrarySong />
             </div>
        </div>
    )
