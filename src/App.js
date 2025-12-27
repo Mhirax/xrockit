f64026b7b1cc3bad36abedfb02a9cf15777af071
Mhirax <olowookeremiracle14@gmail.com>
2025-12-27 08:35:33 +0100

    making changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./styles/app.scss"
 //Adding components 
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
+
 //import utill
 import data from "./util";
 
@@ -21,6 +23,7 @@ function App() {
         setIsPlaying={setIsPlaying}
         isPlaying={isPlaying}
         currentSong={currentSong} />
+      <Library />
     </div>
   );
 }
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,6 @@
 import React from "react";
 import LibrarySong from "./LibrarySongs";
-import { library } from "@fortawesome/fontawesome-svg-core";
+
 
 const = () => {
     return (
