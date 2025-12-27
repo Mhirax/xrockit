0eb56b37b475e6521cca9a918052ca1dd23e196c
Mhirax <olowookeremiracle14@gmail.com>
2025-12-27 08:06:11 +0100

    Exporting our librarysongs

--- src/components/LibrarySongs.js
+++ src/components/LibrarySongs.js
@@ -3,11 +3,11 @@ import React from "react";
 const LibrarySong = ({currentSong}) => {
   return (
     <div className="Librarys-ongs">
-      <img src={currentSong.cover}  />
-      <h2>{currentSong.name}</h2>
-      <h3>{currentSong.artiste}</h3>
+    <img alt={currentSong.name} src={currentSong.cover}></img>  
+      <h3>{currentSong.name}</h2>
+      <h4>{currentSong.artiste}</h3>
     </div>
   );
 };
 
-export default Song;
+export default LibrarySong;
