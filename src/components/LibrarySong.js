fd544a917fdf594466ef764fc07be0044979304f
Mhirax <olowookeremiracle14@gmail.com>
2025-12-30 20:17:44 +0100

    make chamges to my library

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -5,7 +5,7 @@ import React from "react";
 const LibrarySong = ({ song, songs, setCurrentSong, id}) => {
   const songSelectHandler = () => {
     const selectedSong = songs.filter((state) => state.id === id);
-    console.log (id);
+    setCurrentSong(selectedSong[0]);
   }
 
   
