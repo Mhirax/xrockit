45afa56adc06fc2d5398b98bdd1be41d668f76bf
Mhirax <olowookeremiracle14@gmail.com>
2026-01-03 07:42:08 +0100

    added a playpromise

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,7 +2,7 @@ import React from "react";
 import LibrarySong from "./LibrarySong";
 
 
-const Library = ({ songs, setCurrentSong, audioRef}) => {
+const Library = ({ songs, setCurrentSong, audioRef, isPlaying}) => {
     return (
         <div className="library">
             <h2>Library</h2>
@@ -15,6 +15,7 @@ const Library = ({ songs, setCurrentSong, audioRef}) => {
                     id={song.id}
                     key={song.id}
                     audioRef={audioRef}
+                    isPlaying={isPlaying}
                 />
                 ))}
             </div>
