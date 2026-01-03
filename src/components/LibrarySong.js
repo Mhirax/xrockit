de4402694467b6568540a4e9d18877f5a7f6d291
Mhirax <olowookeremiracle14@gmail.com>
2026-01-03 08:00:26 +0100

    sdd active songs to the onclick event

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -21,7 +21,7 @@ const LibrarySong = ({ song, songs, setCurrentSong, id,audioRef, isPlaying}) => 
   
 
   return (
-    <div onClick={songSelectHandler} className="library-song">
+    <div onClick={songSelectHandler} className={`library-song  ${song.active ? 'selected': "" }`}>
       <img alt={song.name} src={song.cover}></img>  
       <div className="song-description">
       <h3>{song.name}</h3>
