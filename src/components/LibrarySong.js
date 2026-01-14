f1a18e438febce53f7ac83037518c084d5698849
Mhirax <olowookeremiracle14@gmail.com>
2026-01-14 16:48:20 +0100

    tracking progress

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,17 +1,34 @@
 import React from "react";
-import { playAudio } from './util';
-
+import { playAudio } from "./util";
 
 
 const LibrarySong = ({ song, songs, setCurrentSong, id,
   audioRef, isPlaying, setSongs }) =>
 {
-  const songSelectHandler = () => {
+  const songSelectHandler = async () => {
     const selectedSong = songs.filter((state) => state.id === id);
-    setCurrentSong(selectedSong[0]);
-   
+    await setCurrentSong(selectedSong[0]);
+
+    //Add active state
+    const newSongs = songs.map((song) => {
+      if (song.id === setCurrentSong.id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+
+    setSongs(newSongs);
+
     //check if the song is playing
-    playAudio(isPlaying, audioRef)
+    playAudio(isPlaying, audioRef);
+
   };
   return (
     <div onClick={songSelectHandler} className={`library-song  ${song.active ? 'selected': "" }`}>
